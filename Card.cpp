59353937627512c2e16c6fbe891f4c9af2b1acc3
end <2053622876@qq.com>
2022-01-17 20:44:33 +0800

    update the definition of the card

--- Card.cpp
+++ Card.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Card.h"
+Card::Card(int uid, string name, int serialNumber)
+{
+    this->uid = uid;
+    this->name = name;
+    this->balance = 0;
+    this->date = defaultDate;
+    int checkNode = 0;
+    //校验码,卡号校验码的计算规则：前6位数字相加的和再模10，得到一个0-9的数，然后用9减去这个数，就是最后一位校验码
+    for (int i = 0; i < 5; ++i)
+    {
+        checkNode += serialNumber % 10;
+        serialNumber /= 10;
+    }
+    checkNode = 9 - (checkNode + 3) % 10;
+    this->cid = (3000000 + serialNumber) * 10 + checkNode;
+}
+void Card::recharge(int amount)
+{
+    if (this->balance + amount > balanceCeiling)
+    {
+        ;
+    }
+    else
+    {
+        this->balance += amount;
+    }
+}
--- Card.h
+++ Card.h
@@ -1,13 +1,23 @@
 //
 // Created by 夏侯臻 on 2022/1/14.
 //
-
+#include <string>
 #ifndef CAMPUSCARDBACKEND_CARD_H
 #define CAMPUSCARDBACKEND_CARD_H
-
-
-class Card {
-
+#define defaultDate 20240715 //默认的卡号有效期
+#define balanceCeiling 999.99 //账户余额上限
+using namespace std;
+class Card
+{
+private:
+    int cid;     //卡号
+    int uid;     //学号
+    string name; //姓名
+    int date;    //有效日期
+    int balance; //账户余额
+public:
+    Card(int, string, int); // card的构造函数
+    void recharge(int);     //充值
 };
 
 
