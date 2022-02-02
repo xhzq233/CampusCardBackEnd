1194a6912e1b0b8588c1cbb422bc721f485aa257
Hayden-J <1761373255@qq.com>
2022-02-02 13:53:41 +0800

    refactor:update file structure

--- Consume.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 夏侯臻 on 2022/1/14.
-//
-
-#include "Consume.h"
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "FileManager.h"
+#include "lib/FileManager.h"
 
 int main() {
     std::vector<std::string> s;
